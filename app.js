584c6d953a793ab15b623ca6b6ffa132ff333ebd
JulianLin549 <sres3416@gmail.com>
2020-08-04 21:57:31 +0800

    add comment feature without styling

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     Campground = require("./models/campground"),
-    seedDB = require("./seeds")
+    Comment = require("./models/comment")
+seedDB = require("./seeds")
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {
     useNewUrlParser: true,
@@ -28,7 +29,7 @@ app.get('/campgrounds', (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.render("index", {
+            res.render("campgrounds/index", {
                 campgrounds: allcampgrounds
             }); //傳入campgrounds property to ejs file
         }
@@ -58,7 +59,7 @@ app.post('/campgrounds', (req, res) => {
 });
 //要比/:id前定義，不然會變成/:id 優先
 app.get('/campgrounds/new', (req, res) => {
-    res.render('new.ejs');
+    res.render('campgrounds/new');
 });
 // SHOW- Shows more info about one campground
 app.get('/campgrounds/:id', (req, res) => {
@@ -71,12 +72,55 @@ app.get('/campgrounds/:id', (req, res) => {
             console.log(err);
         } else {
             //console.log(foundCampground);
-            res.render("show", {
+            res.render("campgrounds/show", {
                 campground: foundCampground
             }); //把回傳的campground傳到ejs裡面。
         }
     });
-})
+});
+
+//======================================
+//COMMENTS ROUTES
+//======================================
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+    //find campground by id
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err)
+        } else {
+            //send to render
+            res.render('comments/new', {
+                campground: campground
+            });
+        }
+    })
+
+});
+
+app.post('/campgrounds/:id/comments', (req, res) => {
+    //lookup campground using ID
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds")
+        } else {
+            Comment.create(req.body.comment, (err, comment) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    //associate comment with campground
+                    campground.comments.push(comment);
+                    campground.save()
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+
+            })
+        }
+        //create new Comment
+        //connect new comment to campground
+        //redirect campground show page
+    });
+});
 const server = app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
--- seeds.js
+++ seeds.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const Comment = require("./models/comment");
-const data = [{
+const seeds = [{
         name: "Cloud Rest",
         image: "https://www.nationalparks.nsw.gov.au/-/media/npws/images/parks/new-england-national-park/thungutti-campground/thunhgutti-campground-01.jpg",
         description: "Nestled under the shade of the tall eucalypt trees, you’ll find plenty of tucked away and secluded campsites to pitch a tent at Thungutti campground.\n Relax and enjoy the lush greenness of scattered ferns around you, listening to the kookaburras laughing in the trees above.\n  Once you get settled, it’s time to explore. Tea Tree Falls walking track starts right here, taking you through tea tree woodland and across the Styx River headwaters to a forest of hanging moss. Or head off on Eagles Nest walking track to discover the incredible natural wonders of the ancient Gondwana rainforest.On your return, you’ll find a barbecue shelter for cooking up a tasty dinner and a place to enjoy the warmth of the campfire. Then, snuggle up for a good night’s rest, before your sunrise visit to Point lookout.",
@@ -16,39 +16,32 @@ const data = [{
         image: "https://www.nationalparks.nsw.gov.au/-/media/npws/images/parks/new-england-national-park/thungutti-campground/thunhgutti-campground-01.jpg",
         description: "Cattai campground is a great place for a weekend getaway with family or friends and a good place to stop with your campervan on a driving tour. \n Once you’ve picked the perfect campsite and pitched your tent, it’s time to start exploring. Kids will love the wide open space that is great for bike riding, playing and running, plus the campground is right near the Hawkesbury River, meaning fishing, canoeing and kayaking opportunities are all nearby. There are shady spots to enjoy a barbecue dinner and the picnic area is nearby for lunchtime activities."
     }
-]
+];
 
 async function seedDB() {
-    //remove all campgrounds
-    await Campground.deleteMany({}, function (err) {
-        if (err) {
-            console.log(err)
-        }
-        console.log("removed campgrounds!")
-    });
-    //add a few campgrounds
-    await data.forEach(function (seed) {
-        Campground.create(seed, function (err, campground) {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log("added campground")
-                //Create comments
-                Comment.create({
-                    text: "This place is Gresy, but I wish there was WIFI",
-                    author: "Homwer Good"
-                }, function (err, comment) { // associate comment with campground
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        campground.comments.push(comment);
-                        campground.save();
-                        console.log("created new comment")
-                    }
+    try {
+        await Comment.deleteMany({});
+        //console.log('comment removed')
+        //remove all campgrounds
+        await Campground.deleteMany({});
+        //console.log('campground removed')
 
-                });
-            }
-        });
-    });
+        for (const seed of seeds) {
+            //add a few campgrounds
+            let campground = await Campground.create(seed);
+            //console.log('campground created')
+            let comment = await Comment.create({
+                text: "This place is Gresy, but I wish there was WIFI",
+                author: "Homwer Good"
+            })
+            //console.log('comment created')
+            //push comment to campgrounds 
+            campground.comments.push(comment);
+            campground.save();
+            //console.log("comment added to campground")
+        }
+    } catch (err) {
+        console.log(err)
+    };
 }
 module.exports = seedDB;
